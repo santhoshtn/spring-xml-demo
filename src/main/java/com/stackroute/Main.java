15fac5ee1ee499c137509d0e4f0a61cbaa1ed813
santhoshtn <santhoshtn1997@gmail.com>
2019-01-15 16:06:17 +0530

    Task 1 created a Maven project with spring xml

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,44 @@
+package com.stackroute;
+
+import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main {
+    public static void main(String[] args){
+      //Actor Using Bean Factory
+
+      BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+      Actor actor=(Actor) beanFactory.getBean("actor1");
+      System.out.println(actor);
+
+      //Movie Using Bean Factory
+
+      Movie movie=(Movie) beanFactory.getBean("movie1");
+      System.out.println(movie);
+
+      //Using BeanDefinitionRegistry and BeanDefinitionReader
+
+      BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
+      BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
+      beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+      Actor actor2= (Actor) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("actor1");
+      System.out.println(actor2);
+
+      //Using ApplicationContext
+
+      ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+      Movie movie2=(Movie) context.getBean("movie1");
+      System.out.println(movie2);
+
+    }
+
+}
---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,20 @@
+package com.stackroute.domain;
+
+public class Movie {
+    private Actor actor;
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actor +
+                '}';
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+}
