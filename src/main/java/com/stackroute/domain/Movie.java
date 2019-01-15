b6e67ad18fabf0ec19d37f47f846246474d15923
santhoshtn <santhoshtn1997@gmail.com>
2019-01-15 17:02:05 +0530

    Task 2 created a Maven project with spring xml

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -7,6 +7,10 @@ public class Movie {
         return actor;
     }
 
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
